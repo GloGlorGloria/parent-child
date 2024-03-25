3fedf3b550af4049d77757c17f0f9ce1df90fe52
GloGlorGloria <gloriabcit@gmail.com>
2024-03-25 14:47:01 -0700

    initial commit

---
+++ .history/pages/index_20240325144329.js
@@ -0,0 +1,14 @@
+
+import styles from "@/styles/Home.module.css";
+
+
+export default function Home() {
+  return (
+    <>
+    
+      <main className={`${styles.main}`}>
+        
+      </main>
+    </>
+  );
+}
